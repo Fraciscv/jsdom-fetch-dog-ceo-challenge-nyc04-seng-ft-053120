9afca8c33d7827dc47079c369641295734156280
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-16 17:16:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,54 @@
-const dogImageContainer = document.querySelector('#dog-image-container');
-const dogBreeds = document.querySelector('#dog-breeds');
-const breedDropdown = document.querySelector('select#breed-dropdown');
-let dogBreedsArray = [];
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const dogImageContainer = document.querySelector('#dog-image-container')
+const dogBreeds = document.querySelector('#dog-breeds')
+const breedDropdown = document.querySelector('select#breed-dropdown')
+let dogBreedsArray = []
 
-fetch('https://dog.ceo/api/breeds/image/random/4')
-    .then(response => response.json())
-    .then(dogImgs => renderDogImgs(dogImgs.message));
+fetch(imgUrl)
+.then(r => r.json())
+.then(dogImgs => renderDogImgs(dogImgs.message))
 
 function renderDogImgs(dogImgs) {
     dogImgs.forEach(dogImgUrl => {
-        const img = document.createElement('img');
-        img.src = dogImgUrl;
-        img.style.height = '300px'
-        dogImageContainer.append(img);
+        const img = document.createElement('img')
+            img.src = dogImgUrl
+            img.className = "center"
+            img.style.height = '300px'
+            dogImageContainer.append(img)
     });
 };
 
-fetch('https://dog.ceo/api/breeds/list/all')
-    .then(response => response.json())
-    .then(dogBreedData => {
-        getDogBreeds(dogBreedData.message)
-        turnDogBreedsArrayToHTML(dogBreedsArray)
+fetch(breedUrl)
+.then(r => r.json())
+.then(dogBreedData => {
+    getDogBreeds(dogBreedData.message)
+    turnDogBreedsArrayToHTML(dogBreedsArray)
     });
 
 function turnDogBreedsArrayToHTML(dogBreedsArray) {
     dogBreedsArray.forEach((dogBreed) => {
-        const newLi = document.createElement('li');
-        newLi.innerText = dogBreed;
-        dogBreeds.append(newLi);
+        const newLi = document.createElement('li')
+            newLi.innerText = dogBreed
+            dogBreeds.append(newLi)
 
-        newLi.addEventListener('click', (e) => {
-            e.target.style.color = 'red';
-        });
-    });
-};
+            newLi.addEventListener('click', (e) => {
+                e.target.style.color = 'red'
+            })
+    })
+}
 
 function getDogBreeds(dogBreedsObject) {
     for (const dogBreed in dogBreedsObject) {
         if (dogBreedsObject[dogBreed].length > 0) {
             dogBreedsObject[dogBreed].forEach((subBreed) => {
-                dogBreedsArray.push(`${subBreed} ${dogBreed}`)
-            });
+            dogBreedsArray.push(`${subBreed} ${dogBreed}`)
+        })
         } else {
-            dogBreedsArray.push(dogBreed);
-        };
-    };
-};
+            dogBreedsArray.push(dogBreed)
+        }
+    }
+}
 
 breedDropdown.addEventListener('change', (e) => {
     dogBreeds.innerHTML = ""
